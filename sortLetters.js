dae8a3297f421dfdf6bb04457eca7ce227bd08d0
M.ngugi <mngugi7@gmail.com>
2024-04-18 11:43:29 +0300

    Update sortLetters.js

--- sortLetters.js
+++ sortLetters.js
@@ -1,8 +1,16 @@
-const data = {'A','B', 'C'}
-
+const data = {'A','B', 'C'} 
+const data2 = {'d','e', 'f'} 
 
 for (let i = 0; i < data.length; i++ ) {
   console.log(data[i])
 
 
 }
+
+for (let j = 0; j < data2.length; j++ ) {
+    console.log(data2[j])
+  
+  
+  }
+
+O(n * a)
